dd72d398e4ac872acf956a0cb988b89bceb8497b
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-07-14 22:05:35 +0100

    Newly_add

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,7 +5,6 @@ int main(void)
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
 p = &n;
 /**
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * infinite_add - adds two numbers
  * @n1: first number
