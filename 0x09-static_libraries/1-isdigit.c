5411794ec3fcd8277cbd842dc5fe67ee9f9c5e4e
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-07-22 16:42:44 +0100

    Mine

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -8,12 +8,12 @@
 
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -8,5 +8,5 @@
 
 int _islower(int c)
 {
-return (c >= 97 && c <= 122);
+	return (c >= 97 && c <= 122);
 }
