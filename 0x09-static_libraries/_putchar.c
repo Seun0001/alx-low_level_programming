66491cacf9f56c6615e21f0d53288259d613fa7a
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-07-22 16:14:52 +0100

    new

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,8 +1,7 @@
-
 #include <unistd.h>
 
 /**
- * _putchar - writes char c to std out 
+ * _putchar - writes char c to std out
  * @c: char to print
  * Return: 1 on success. * on error, -1 is returned, and errno is done.
  */
