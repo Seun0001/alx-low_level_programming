bd4966081638b0d0ad1f9b044dfaad7fdb562b91
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-07-28 15:33:56 +0100

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,5 +12,10 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
+int *int_calloc(int nmemb, unsigned int size);
+void mult(int *product, char *n1, char *n2, int len1, int len2);
+int is_valid(char *num);
+void err(int status);
+int main(int argc, char **argv);
 
 #endif /* _MAIN_H */
