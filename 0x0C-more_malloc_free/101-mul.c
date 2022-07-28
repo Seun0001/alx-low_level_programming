85a7ed5544f336b690e40ae931d7881ac7e1914f
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-07-28 14:02:38 +0100

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,126 +1,138 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
-
+#include <stdlib.h>
+#include <cytpe.h>
 /**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
- *
- * Return: no return.
- */
-void _is_zero(char *argv[])
+  * int_calloc - special calloc but 4 int arrays
+  * @nmemb: n memb
+  * @size: size of array
+  * Return: int *
+  */
+int *int_calloc(int nmemb, unsigned int size)
 {
-int i, isn1 = 1, isn2 = 1;
-
-for (i = 0; argv[1][i]; i++)
-if (argv[1][i] != '0')
-{
-isn1 = 0;
-break;
-}
-
-for (i = 0; argv[2][i]; i++)
-if (argv[2][i] != '0')
-{
-isn2 = 0;
-break;
-}
-
-if (isn1 == 1 || isn2 == 1)
-{
-printf("0\n");
-exit(0);
-}
+	/* declarations */
+	int *p, n;
+	/* checking inputs */
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	/* malloc the space & check for fail */
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	/* calloc */
+	for (n = 0; n < nmemb; n++)
+		p[n] = 0;
+	return (p);
 }
 
 /**
- * _initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- *
- * Return: pointer of a char array.
- */
-char *_initialize_array(char *ar, int lar)
+  * mult - multiplication
+  * @product: int * 4 answer
+  * @n1: string num1
+  * @n2: string num2
+  * @len1: len num1
+  * @len2: len num2
+  * Return: void
+  */
+void mult(int *product, char *n1, char *n2, int len1, int len2)
 {
-int i = 0;
-
-for (i = 0; i < lar; i++)
-ar[i] = '0';
-ar[lar] = '\0';
-return (ar);
+	/* declarations */
+	int i;
+	int j;
+	int f1, f2;
+	int sum;
+	/* the long math */
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		sum = 0;
+		f1 = n1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			f2 = n2[j] - '0';
+			sum += product[i + j + 1] + (f1 * f2);
+			product[i + j + 1] = sum % 10;
+			sum /= 10;
+		}
+		if (sum > 0)
+			product[i + j + 1] += sum;
+	}
+	for (i = 0; product[i] == 0 && i < len1 + len2; i++)
+	{}
+	if (i == len1 + len2)
+		_putchar('0');
+	for (; i < len1 + len2; i++)
+		_putchar(product[i] + '0');
+	_putchar('\n');
 }
 
 /**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
-int _checknum(char *argv[], int n)
+  * is_valid - is the number a valid one
+  * @num : char string num
+  * Return: int, 1 if true 0 if false
+  */
+int is_valid(char *num)
 {
-int ln;
-
-for (ln = 0; argv[n][ln]; ln++)
-if (!isdigit(argv[n][ln]))
-{
-printf("Error\n");
-exit(98);
+	/* declarations */
+	int i;
+	/* checking for ints */
+	for (i = 0; num[i]; i++)
+	{
+		if (num[i] < '0' || num[i] > '9')
+			return (0);
+	}
+	return (1);
 }
-
-return (ln);
-}
-
 /**
- * main - Entry point.
- * program that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: arguments vector.
- *
- * Return: 0 - success.
- */
-int main(int argc, char *argv[])
+  * err - errors r us
+  * @status: error code 4 exit
+  * Return: void
+  */
+void err(int status)
 {
-int ln1, ln2, lnout, add, addl, i, j, k, ca;
-char *nout;
-
-if (argc != 3)
-printf("Error\n"), exit(98);
-ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-if (nout == NULL)
-printf("Error\n"), exit(98);
-nout = _initialize_array(nout, lnout);
-k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-for (; k >= 0; k--, i--)
-{
-if (i < 0)
-{
-if (addl > 0)
-{
-add = (nout[k] - '0') + addl;
-if (add > 9)
-nout[k - 1] = (add / 10) + '0';
-nout[k] = (add % 10) + '0';
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(98);
 }
-i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
-}
-if (j < 0)
+/**
+  * main - getting the args
+  * @argc: args #
+  * @argv: arg array
+  * Return: 0
+  */
+int main(int argc, char **argv)
 {
-if (nout[0] != '0')
-break;
-lnout--;
-free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-}
-if (j >= 0)
-{
-add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-addl = add / 10, nout[k] = (add % 10) + '0';
-}
-}
-printf("%s\n", nout);
-return (0);
+	/* declarations */
+	int i, j, len1 = 0, len2 = 0;
+	int *res;
+	/* too many args? too few? */
+	if (argc != 3)
+	{
+		err(98);
+	}
+	/* using isvalid */
+	for (i = 1; i < argc; i++)
+	{
+		if (!(is_valid(argv[i])))
+			err(98);
+		if (i == 1)
+		{
+			for (j = 0; argv[i][j]; j++)
+				len1++;
+		}
+		if (i == 2)
+		{
+			for (j = 0; argv[i][j]; j++)
+				len2++;
+		}
+	}
+	res = int_calloc(len1 + len2, sizeof(int));
+	if (res == NULL)
+		err(98);
+	mult(res, argv[1], argv[2], len1, len2);
+	free(res);
+	return (0);
 }
