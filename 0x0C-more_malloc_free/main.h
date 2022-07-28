a6b8462b683b41fe65f1a8b2c571acb0e66dcdc6
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-07-28 16:00:10 +0100

    Trust no one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,6 +10,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
 int *int_calloc(int nmemb, unsigned int size);
 void mult(int *product, char *n1, char *n2, int len1, int len2);
 int is_valid(char *num);
