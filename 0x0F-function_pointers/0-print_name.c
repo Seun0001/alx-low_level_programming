f92d1bcff6a3283692a8fd08b51609ee486e2135
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-08-03 12:59:49 +0100

    What's my name?

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name - name.
+ * return: 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	print_name char;
+	(char*) = name;
+	void (*f)(char *)
+	{
+		(*f)(char *)
+	}
+	return (print_name);
+};
+int main(void)
+{
+	prints_name char;
+	*char = Bobby;
+
+	printf("my name is %s\n", char);
+	return 0;
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /*_FUNCTION_POINTERS_H*/
