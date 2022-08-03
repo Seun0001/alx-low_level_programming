37affac841373088187eca5d967cbd1fc2139452
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-08-03 13:17:17 +0100

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,15 +5,21 @@
 /**
  * print_name - function that prints a name.
  * @name - name.
- * return: name.
+ * return: 0.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	print_name char;
 	(char*) = name;
-	name = Bobby;
 	print_name("may name is %s", name);
 
 	return (name);
 };
+
+int main(void)
+{
+	printf("my name is");
+	scanf("%s", @name);
+	return 0;
+}
