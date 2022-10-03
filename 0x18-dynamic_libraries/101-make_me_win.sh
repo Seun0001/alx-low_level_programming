547b84ec11249bba715c7072dbb435097f0b9574
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-10-03 16:30:16 +0100

    Add

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gcc *.o -shared -o libruin.so
-export LD_PRELOAD=$PWD/libruin.so
+wget -P /tmp/ https://raw.github.com./Seun0001/alx-low_level_programming/main/0x18-dynamic_libraries/libruin.so
+export LD_PRELOAD="$PWD/libruin.so"
