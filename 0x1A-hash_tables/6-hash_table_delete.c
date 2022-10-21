caa016662b704c06219a01295c1d601b0bb90f70
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-10-21 14:26:35 +0100

    All

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,66 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * free_node - Free a node.
- * @node: Node to free.
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
  *
- * Return: Void.
+ * Return: void
  */
-void free_node(hash_node_t *node)
+void hash_table_delete(hash_table_t *ht)
 {
-	free(node->key);
-	free(node->value);
-	free(node);
-}
+	unsigned long int i;
+	hash_node_t *next;
 
-/**
- * hash_table_set - Set a value in the hash table.
- * @ht: Hash table.
- * @key: Key to be indexed.
- * @value: Value to set in the hash table.
- *
- * Return: 1 if works, 0 if doesn't.
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
-	hash_node_t *new_node, *current;
-
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup((char *)key);
-	new_node->value = strdup((char *)value);
-	new_node->next = NULL;
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
-	else
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+	for (i = 0; i < ht->size; i++)
 	{
-		current = ht->array[index];
-		if (strcmp(current->key, key) == 0)
+		while (ht->array[i] != NULL)
 		{
-			new_node->next = current->next;
-			ht->array[index] = new_node;
-			free_node(current);
-			return (1);
-		}
-		while (current->next != NULL && strcmp(current->next->key, key) != 0)
-		{ current = current->next;
-		}
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next->next;
-			free_node(current->next);
-			current->next = new_node;
-		}
-		else
-		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
+			next = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = next;
 		}
 	}
-	return (1);
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -82,4 +82,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif
+#endif /* _HASH_TABLES_H_ */
